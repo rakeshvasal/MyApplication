758bd90890c2b703a1019fa5551edb589348f0e7
Axisvation <axisvationtech@gmail.com>
2017-11-10 19:27:24 +0530

    user master created

--- app/src/main/java/com/example/rakeshvasal/myapplication/Fragments/UserMasterFragment.java
+++ app/src/main/java/com/example/rakeshvasal/myapplication/Fragments/UserMasterFragment.java
@@ -1,6 +1,7 @@
 package com.example.rakeshvasal.myapplication.Fragments;
 
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -19,6 +20,7 @@ import com.example.rakeshvasal.myapplication.Custom_Adapters.UserMasterAdapter;
 import com.example.rakeshvasal.myapplication.GetterSetter.Events;
 import com.example.rakeshvasal.myapplication.GetterSetter.User;
 import com.example.rakeshvasal.myapplication.R;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -64,8 +66,25 @@ public class UserMasterFragment extends BaseFragment {
             e.printStackTrace();
         }
 
-        fetchallusers();
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getActivity().getFragmentManager().beginTransaction();
+                android.app.Fragment fragment = new AddUpdateUserFragment();
+                transaction.replace(R.id.fragment_container, fragment);
+                transaction.commit();
+            }
+        });
 
+        btn_search.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        fetchallusers();
+        readData();
         return rootview;
     }
 
@@ -97,4 +116,59 @@ public class UserMasterFragment extends BaseFragment {
             e.printStackTrace();
         }
     }
+
+    private void readData(){
+        DatabaseReference childref = ref.child("").getRef();
+        ref.orderByChild("user_name").equalTo("Rakesh Vasal").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String prevChildKey) {
+                User user_name = dataSnapshot.getValue(User.class);
+                //System.out.println(dataSnapshot.getKey() + " was " + dinosaur.height + " meters tall.");
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+
+
+        });
+
+
+        childref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot eventsnapshot : dataSnapshot.getChildren()) {
+                    Log.d("children",dataSnapshot.getChildren().toString());
+                    //User user = eventsnapshot.getValue(User.class);
+                    //mUserEntries.add(user);
+
+                }
+                /*UserMasterAdapter adapter = new UserMasterAdapter(getActivity(),mUserEntries);
+                recyclerView.setAdapter(adapter);
+                adapter.notifyDataSetChanged();*/
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.d("usermasterdatabaseerror", databaseError.getMessage());
+            }
+        });
+    }
 }
+
