6d3b5aa001e655d5616e374a80bbf63ce81f6990
Axisvation <axisvationtech@gmail.com>
2017-12-01 19:02:25 +0530

    user master created

--- app/src/main/java/com/example/rakeshvasal/myapplication/Fragments/TopMoviesFragment.java
+++ app/src/main/java/com/example/rakeshvasal/myapplication/Fragments/TopMoviesFragment.java
@@ -3,6 +3,9 @@ package com.example.rakeshvasal.myapplication.Fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -20,6 +23,7 @@ public class TopMoviesFragment extends BaseFragment {
         // Required empty public constructor
     }
 
+    RecyclerView recyclerView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -27,7 +31,10 @@ public class TopMoviesFragment extends BaseFragment {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_top_movies, container, false);
 
-
+        recyclerView = (RecyclerView) root.findViewById(R.id.recycler_view);
+        RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(getActivity(), 1);
+        recyclerView.setLayoutManager(mLayoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
 
         return root;
     }
