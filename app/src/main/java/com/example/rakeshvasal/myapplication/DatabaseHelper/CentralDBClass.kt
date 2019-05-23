1b8d569605c2028259a947fa50e79a1602b3abe3
rakeshvasal <rakeshvasal@cardinbox.com>
2019-05-23 19:12:12 +0530

    started room db

--- app/src/main/java/com/example/rakeshvasal/myapplication/DatabaseHelper/CentralDBClass.kt
+++ app/src/main/java/com/example/rakeshvasal/myapplication/DatabaseHelper/CentralDBClass.kt
@@ -1,3 +1,31 @@
 package com.example.rakeshvasal.myapplication.DatabaseHelper
 
-class CentralDBClass
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.rakeshvasal.myapplication.DbUser
+
+@Database(
+        entities = [DbUser::class],
+        version = 1
+)
+
+abstract class CentralDBClass : RoomDatabase() {
+
+    companion object {
+        var Instance: CentralDBClass? = null
+
+        fun getDatabaseObject(context: Context): CentralDBClass? {
+
+            if (Instance == null) {
+                synchronized(CentralDBClass::class) {
+                    Instance = Room.databaseBuilder(context.applicationContext, CentralDBClass::class.java, "CentralDb")
+                            .build()
+                }
+            }
+            return Instance
+        }
+
+    }
+}
